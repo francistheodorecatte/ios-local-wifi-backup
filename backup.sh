ac5e067a9b92c388c4d1d230c740ef7f33ad8f85
Francis Theodore Catte <14878852+francistheodorecatte@users.noreply.github.com>
2026-01-17 16:12:30 -0500

    add UUID input to allow backing up multiple different devices

--- backup.sh
+++ backup.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
+# usage: backup.sh <UUID> <IP>
+
 if [ -z "$1" ]; then
-	echo "no ip address provided!" >&2
-	exit 1
+        echo "no device UUID provided!" >&2
+        exit 1
 fi
-IP_ADDRESS="$1"
+DEV_UUID="$1"
+
+if [ -z "$2" ]; then
+        echo "no ip address provided!" >&2
+        exit 1
+fi
+IP_ADDRESS="$2"
+
 # see: https://github.com/fosple/usbmuxd2/blob/master/README.md
 service dbus start && service avahi-daemon start
 sleep 1
 
-
 PLIST_DIR="/var/lib/lockdown"
-plist_files=("$PLIST_DIR"/*.plist)
+plist_files=("$PLIST_DIR/$DEV_UUID.plist" "$PLIST_DIR/SystemConfiguration.plist")
 
 if [ "${#plist_files[@]}" -ne 2 ]; then
-  echo "Error: Expected exactly 2 plist files, found ${#plist_files[@]}"
+  echo "Error: Expected 2 plist files, found ${#plist_files[@]}"
   exit 1
 fi
 
-# Sort to ensure alphabetical order (UUID first, SystemConfiguration second)
-IFS=$'\n' sorted_plists=($(sort <<<"${plist_files[*]}"))
-unset IFS
-
-uuid_plist="$(basename "${sorted_plists[0]}" .plist)"
-system_config_plist="${sorted_plists[1]}"
+uuid_plist="$(basename "${plist_files[0]}" .plist)"
+system_config_plist="${plist_files[1]}"
 
 if [[ "$(basename "$system_config_plist")" != "SystemConfiguration.plist" ]]; then
-  echo "Error: Second plist must be SystemConfiguration.plist"
+  echo "Error: Second plist must be SystemConfiguration.plist (did you not supply the correct UUID?)"
   exit 1
 fi
 
@@ -38,4 +42,3 @@ PID=$!
 sleep 2
 idevicebackup2 -in backup --full /backup
 kill $PID
-# scp * springfield:~/iPhoneBackup/

581fb3f69242a290b6035b931edb0c3f0f17529e
Francis Theodore Catte <14878852+francistheodorecatte@users.noreply.github.com>
2026-01-17 19:27:31 -0500

    print device UUID before exiting if pairing successful
    script also automatically exits instead of spinning in a while loop forever

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
+
 # see: https://github.com/fosple/usbmuxd2/blob/master/README.md
 service dbus start && service avahi-daemon start
 sleep 2
 
 usbmuxd -d -z &
-while : ; do
-	sleep 10
-	ideviceinfo
-done
+
+sleep 10
+ideviceinfo
+
+UUID=$(idevice_id -l)
+if [ -f "/var/lib/lockdown/$UUID.plist" ]; then
+        echo -e "\nNewly paired device ID is: $UUID"
+        exit 0
+fi
+
+exit 1
